16c56fd4d5c1e9a54617c8cd2156bf67849530fc
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-07-24 19:06:40 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,14 +1,33 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+    # extract the html data with each student's information
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    students = doc.css(".student-card")
     
+    # parse the data and organize name, location and profile url for each student in an array
+    student_arr = []
+    
+    students.each do |student|
+      name = student.css(".card-text-container").css("h4").text
+      location = student.css(".card-text-container").css("p").text
+      profile_url = student.css("a").first.first.last
+      new_student = {:name => name, :location => location, :profile_url => profile_url}
+      student_arr.push(new_student)
+    end
+    
+    student_arr # return this array, where each element is a hash with individual student info
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+    binding.pry
   end
 
 end
