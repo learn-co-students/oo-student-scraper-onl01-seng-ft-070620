b8a282dc8f3deef8fefd8403c159af3061076acb
JacksonReynolds <jwreynold@gmail.com>
2020-07-24 18:25:11 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,9 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    attributes_hash.each{|key, value| self.send(("#{key}="), value)}
+    attributes_hash.each do |key, value| 
+      self.send(("#{key}="), value)
+    end #do
   end
 
   def self.all
