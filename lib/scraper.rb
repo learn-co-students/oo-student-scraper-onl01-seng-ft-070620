c90762fe3930bbb04573e650795f560c89eb98c3
JacksonReynolds <jwreynold@gmail.com>
2020-07-24 17:38:43 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,16 +6,28 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
-    doc.css(".student-card").each do |card|
-      profile_ext = card.css('a').attribute('href').text
-      pself.scrape_profile_page(index_url + profile_ext)
-      # binding.pry
+    arr = doc.css(".student-card").collect do |card|
+      student = {}
+      student[:name] = card.css('.student-name').text
+      student[:location] = card.css('.student-location').text
+      student[:profile_url] = card.css('a').attribute('href').text
+      student
     end # do
-  end
+  end #self.scrape_index_page
 
   def self.scrape_profile_page(profile_url)
-    
-  end
+    student = {}
+    doc = Nokogiri::HTML(open(profile_url))
+    student[:profile_quote] = doc.css('.profile-quote').text
+    student[:bio] = doc.css('.bio-content div.description-holder').text.strip
+    doc.css('.social-icon-container a').each do |icon|
+      netwrk = icon.css('img').attribute('src').text.gsub('../assets/img/', '').split('-')[0]
+      netwrk = 'blog' if netwrk == 'rss'
+      student[netwrk.to_sym] = icon.attribute('href').text
+    end #do
+    student
+
+  end # self.scrape_profile_page
 
 end
 
