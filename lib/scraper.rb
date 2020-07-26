0c2668df1aa35fed885f1d6f1b4aac19182f38fb
cTraymany <chindalatht@gmail.com>
2020-07-26 23:36:04 +0700

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,52 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
-
+  
   def self.scrape_index_page(index_url)
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+
+    students = []
     
+    doc.css("div.student-card").each do |student| # for each student-card, do below
+      name = student.css(".student-name").text # in each student-card, select their name
+      location = student.css(".student-location").text # in each student-card, select their location
+      profile_url = student.css("a").attribute("href").value # placeholder -- find their profile link
+
+      each_student = { #for each student-card, create hash with the variables retrieved
+        name: name,
+        location: location,
+        profile_url: profile_url
+      }
+      students << each_student #shovel each student hash into an array
+    end
+    students #return that array
   end
 
   def self.scrape_profile_page(profile_url)
-    
-  end
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
 
+    student_data = {}
+    
+    socials = doc.css(".social-icon-container a")#.attribute("href").value
+    socials.each do |link|
+      # binding.pry
+      if link.attribute("href").value.include?("twitter")
+        student_data[:twitter] = link.attribute("href").value
+      elsif link.attribute("href").value.include?("linkedin")
+        student_data[:linkedin] = link.attribute("href").value
+      elsif link.attribute("href").value.include?("github")
+        student_data[:github] = link.attribute("href").value
+      elsif link.attribute("href").value.include?(".com")
+        student_data[:blog] = link.attribute("href").value
+      end
+      student_data[:profile_quote] = doc.css(".profile-quote").text
+      student_data[:bio] = doc.css(".description-holder p").text
+    end
+    student_data
+  end
 end
 
--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,39 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    # binding.pry
+    @name = student_hash[:name] unless student_hash[:name] == nil
+    @location = student_hash[:location] unless student_hash[:location] == nil
+    @twitter = student_hash[:twitter] unless student_hash[:twitter] == nil
+    @linkedin = student_hash[:linkedin] unless student_hash[:linkedin] == nil
+    @github = student_hash[:github] unless student_hash[:github] == nil
+    @blog = student_hash[:blog] unless student_hash[:blog] == nil
+    @profile_quote = student_hash[:profile_quote] unless student_hash[:profile_quote] == nil
+    profile_url = student_hash[:profile_url] unless student_hash[:profile_url] == nil
+    self.class.all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student|
+      # binding.pry
+      Student.new(student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    # self.bio to work
+    # binding.pry
+    self.twitter = attributes_hash[:twitter]
+    self.linkedin = attributes_hash[:linkedin]
+    self.github = attributes_hash[:github]
+    self.profile_quote = attributes_hash[:profile_quote]
+    self.bio = attributes_hash[:bio]
+    self.blog = attributes_hash[:blog]
+
   end
 
   def self.all
-    
+    @@all
   end
 end
 
